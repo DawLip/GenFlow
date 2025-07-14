5bdd8772e70c9c2acfe90db64eddab4ece2ffe04
DawLip <d.lipinski022@gmail.com>
2025-07-14 05:39:40 +0200

    fix: hotfix

--- services/graphql/src/main.ts
+++ services/graphql/src/main.ts
@@ -4,6 +4,8 @@ import { Logger } from 'nestjs-pino';
 
 import { name } from '../package.json';
 import { service_name } from '@shared/service_name'
+import { DefaultExceptionFilter } from '@libs/shared/src/default-exception.filter';
+import { GrpcExceptionFilter } from '@libs/shared/src/grpc-exception.filter';
 
 const s_name = service_name(name);
 
@@ -15,6 +17,10 @@ async function bootstrap() {
   app.enableCors();
 
   app.useLogger(app.get(Logger));
+
+  app.useGlobalFilters(new DefaultExceptionFilter(app.get(Logger)));
+  app.useGlobalFilters(new GrpcExceptionFilter(app.get(Logger)));
+
   await app.listen(port);
   console.log(`${s_name} service running on http://localhost:${port}`);
 }

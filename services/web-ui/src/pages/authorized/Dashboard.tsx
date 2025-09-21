7ee4989ea7692baadf1aa38d6717514ed2b62b57
DawLip <d.lipinski022@gmail.com>
2025-09-21 08:46:06 +0200

    feat(web-ui): new view for dashboard and selectProject

--- services/web-ui/src/pages/authorized/Dashboard.tsx
+++ services/web-ui/src/pages/authorized/Dashboard.tsx
@@ -6,6 +6,7 @@ import { AppDispatch } from "@web-ui/store";
 import { setFlow } from "@web-ui/store/slices/flowsSlice";
 import { selectFlow } from "@web-ui/store/slices/sessionSlice";
 import { createFlowThunk } from "@web-ui/store/thunks/flow/createFlowThunk";
+import { selectFlowThunk } from "@web-ui/store/thunks/project/selectFlowThunk";
 import { useWebRTC } from "@web-ui/webrtc/webrtc.context";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -15,9 +16,8 @@ import { useDispatch, useSelector } from "react-redux";
 
 export default function Page() {
   const dispatch = useDispatch<AppDispatch>();
-  const flows = useSelector((state: any) => state.project.flows);
-  // const { mediaStream, setMediaStream } = useWebRTC();
-  // console.log("test>>>>>>>>>>>>>>>>>>>>>>>>>>>: ", mediaStream);
+  const flows = useSelector((state: any) => state.projectRepo.projects.filter(p=>p.name===state.projectRepo.selectedProject)[0]?.flows || [])
+
   return (
     <div className="self-stretch flex-1 px-8 pt-8 justify-start items-start gap-16 overflow-hidden">
       <div className="flex-1 flex-col justify-center items-start gap-16">
@@ -33,9 +33,12 @@ export default function Page() {
           </div>
           <div className="self-stretch flex flex-col justify-start items-start">
             <div className="self-stretch flex-col justify-start items-start gap-2">
-              {Object.keys(flows).length === 0 
-                ? <div>No flows found</div>
-                : Object.keys(flows).reverse().map((flowID, i) => i < 5 && <FlowCard key={flowID} name={flows[flowID].name} path={flows[flowID].path} />)
+              {
+                flows.length > 0
+                ? flows.map((flow: any) => (
+                  <FlowCard key={flow} name={flow} />
+                ))
+                : <div>No flows found</div>
               }
             </div>
           </div>
@@ -46,17 +49,10 @@ export default function Page() {
   );
 }
 
-const FlowCard = ({name, path}:{name:string, path:string}) => {
+const FlowCard = ({name}:{name:string}) => {
+  const webRTC = useWebRTC();
   const router = useRouter()
   const dispatch = useDispatch<AppDispatch>();
-
-  const userId = useSelector((state: any) => state.client.userId);
-  const projectId = useSelector((state: any) => state.project.projectId);
-  const flowID = projectId + ':' + path + name;
-
-  const flow = useSelector((state: any) => state.project.flows.filter((flow: any) => flow.name === name)[0]);
-  const socket = useSocket();
-  if(!socket) return;
 
   return (
     <div className="self-stretch flex-col justify-start items-start gap-4">
@@ -67,19 +63,8 @@ const FlowCard = ({name, path}:{name:string, path:string}) => {
           </div>
           <div>
             <Icon name="flow" className="size-[24px]" onClick={async () => {
-                path = path.replace(/^\/|\/$/g, "")=="" ? "/" : path.replace(/^\/|\/$/g, "") + '/';
-                const url = `${services_config.service_url.gateway_web_ui}/api/projects/${projectId}/flows/${path ? `${path}/` : ""}${name}`;
-                console.log("Fetching flow from URL:", url);
-                const {data} = await axios.get(url);
-                dispatch(setFlow({flowID, data: data.flow}));
-                dispatch(selectFlow(flowID));
-                socket.emit('join_flow_room',{
-                  projectId: projectId,
-                  flowName: name,
-                  flowPath: path,
-                  userId: userId
-                });
-                router.push('/flow');
+                dispatch(selectFlowThunk(webRTC, name));
+                router.push('/flow')
               }}/>
           </div>
         </div>
--- services/web-ui/src/pages/authorized/SelectProject.tsx
+++ services/web-ui/src/pages/authorized/SelectProject.tsx
@@ -1,32 +1,70 @@
 
+import { Icon } from "@web-ui/components/Icon"
 import { AppDispatch } from "@web-ui/store"
 import { selectProject } from "@web-ui/store/slices/projectsRepoSlice"
+import { createFlowThunk } from "@web-ui/store/thunks/project/createFlowThunk"
 import { getProjectConfigThunk } from "@web-ui/store/thunks/project/getProjectConfigThunk"
 import { useWebRTC } from "@web-ui/webrtc/webrtc.context"
 import { useRouter } from "next/navigation"
 import { useDispatch, useSelector } from "react-redux"
 
 export default function Page() {
-  const router  = useRouter()
   const dispatch = useDispatch<AppDispatch>();
   const projects = useSelector((state:any) => state.projectRepo.projects)
-  const webRTC = useWebRTC();
-  
+
   return (
-    <div>
-      {projects.map((project:any) => (
-        <div 
-          key={project.name} 
-          className="cursor-pointer"
-          onClick={() => {
-            dispatch(selectProject(project.name))
-            dispatch(getProjectConfigThunk(webRTC, project.name, project.genworkerStorageId))
-            router.push('/dashboard')
-          }}
-        >
-          {project.name}
+    <div className="self-stretch flex-1 px-8 pt-8 justify-start items-start gap-16 overflow-hidden">
+      <div className="flex-1 flex-col justify-center items-start gap-16">
+        <div className="self-stretch flex flex-col justify-start items-start gap-4">
+          <div className="self-stretch justify-between items-center">
+            <div className="justify-start text-on_bg/80 text-5xl font-bold font-['Inter'] leading-[48px]">
+              Select project
+            </div>
+            <div className="flex justify-start items-center gap-4">
+              <Icon name="plus" onClick={() => {}} />
+            </div>
+          </div>
+          <div className="self-stretch flex flex-col justify-start items-start">
+            <div className="self-stretch flex-col justify-start items-start gap-2">
+              {
+                projects.length > 0
+                ? projects.map((project: any) => (
+                  <FlowCard key={project.name} name={project.name} genworkerStorageId={project.genworkerStorageId}/>
+                ))
+                : <div>No projects found</div>
+              }
+            </div>
+          </div>
         </div>
-      ))}
+      </div>
+      <div className="flex-1 h-48" />
     </div>
-  )
+  );
+}
+
+const FlowCard = ({name, genworkerStorageId}:{name:string, genworkerStorageId:string}) => {
+  const webRTC = useWebRTC();
+  const router = useRouter()
+  const dispatch = useDispatch<AppDispatch>();
+
+  const onSelectProject = () => {
+    dispatch(selectProject(name))
+    dispatch(getProjectConfigThunk(webRTC, name, genworkerStorageId))
+    router.push('/dashboard')
+  }
+
+  return (
+    <div className="cursor-pointer self-stretch flex-col justify-start items-start gap-4" onClick={() => onSelectProject()}>
+      <div className="self-stretch flex-col justify-start items-start gap-2">
+        <div className="self-stretch justify-between items-center">
+          <div className="justify-start text-on_bg/80 text-base font-normal font-['Inter'] leading-none">
+            {name}
+          </div>
+          <div>
+            <Icon name="flow" className="size-[24px]"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
